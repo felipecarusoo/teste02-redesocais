271ecfd2f27757dbce7c250e8b590f0b3eb0a7d6
Wonder <101845224+felipecarusoo@users.noreply.github.com>
2024-10-30 07:38:41 -0300

    .

--- usuariosrs.js
+++ usuariosrs.js
@@ -3,7 +3,7 @@ const url = 'https://raw.githubusercontent.com/teuzsw/graficos-alura/refs/heads/
 async function visualizarInformacoes() {
     const resp = await fetch(url)
     const dados = resp.json()
-    console.log(dados);
+    console.log(dados)
     
     
 }
