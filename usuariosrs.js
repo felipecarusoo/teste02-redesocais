ab8faa51278b07914d91554eb45ab5d54efdc65e
Wonder <101845224+felipecarusoo@users.noreply.github.com>
2024-10-30 07:50:50 -0300

    .

--- usuariosrs.js
+++ usuariosrs.js
@@ -7,7 +7,7 @@ async function visualizarInformacoes() {
     
     const paragrafo = document.createElement('p')
 
-    paragrafo.innerHTML = `Você sabia que existem <span>${dados.Facebook}</span> usuários ativos no facebook?`
+    paragrafo.innerHTML = `Você sabia que existem <span>${dados.Facebook}</span> de usuários ativos no facebook?`
     console.log(paragrafo)
 
     const container = document.getElementById('graficos-container')
